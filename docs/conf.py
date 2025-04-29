6c5733d1fc2d9da9183a526ec52acc20977a0a83
Thalia Valle <tvallechavez4390@eagle.fgcu.edu>
2025-04-29 01:19:10 -0400

    updating RTD build configuration

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../src"))
-import src.maritimeviz as maritimeviz
+import maritimeviz
 
 # -- Project information -----------------------------------------------------
 
